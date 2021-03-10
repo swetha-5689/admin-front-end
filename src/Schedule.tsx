792ce263b91c7ee335bd8d837e78cbe0aa87c0c2
Swetha Angara <swetha.angara5689@gmail.com>
2021-03-10 17:20:28 -0500

    Added approve/reject button to requests

--- src/Schedule.tsx
+++ src/Schedule.tsx
@@ -1,97 +1,115 @@
-import { getDate } from 'date-fns';
 import React, { useState } from "react";
 import "./Schedule.css";
 import logo from "./assets/logo-qs.png";
-import { Badge, Calendar, Dropdown, Icon, Nav, Popover, Whisper } from "rsuite";
-import { AppHeader } from '@commure/components-core';
-import { PractitionerList } from './components/PractitionerList';
-import PractitionerService from './ScheduleService';
-import ScheduleService from './ScheduleService';
-function getTodoList(date: any) {
-  const day = getDate(date);
-  switch (day) {
-    case 10:
-      return [
-        { time: "10:30 am", title: "Meeting" },
-        { time: "12:00 pm", title: "Lunch" },
-      ];
-    case 15:
-      return [
-        { time: "09:30 pm", title: "Products Introduction Meeting" },
-        { time: "12:30 pm", title: "Client entertaining" },
-        { time: "02:00 pm", title: "Product design discussion" },
-        { time: "05:00 pm", title: "Product test and acceptance" },
-        { time: "06:30 pm", title: "Reporting" },
-        { time: "10:00 pm", title: "Going home to walk the dog" },
-      ];
-    default:
-      return [];
-  }
-}
-
-function renderCell(date: any) {
-  const list = getTodoList(date);
-  const displayList = list.filter((item, index) => index < 2);
-
-  if (list.length) {
-    const moreCount = list.length - displayList.length;
-    const moreItem = (
-      <li>
-        <Whisper
-          placement="top"
-          trigger="click"
-          speaker={
-            <Popover>
-              {list.map((item, index) => (
-                <p key={index}>
-                  <b>{item.time}</b> - {item.title}
-                </p>
-              ))}
-            </Popover>
-          }
-        >
-          <a>{moreCount} more</a>
-        </Whisper>
-      </li>
-    );
-
-    return (
-      <ul>
-        {displayList.map((item, index) => (
-          <li key={index}>
-            <Badge /> <b>{item.time}</b> - {item.title}
-          </li>
-        ))}
-        {moreCount ? moreItem : null}
-      </ul>
-    );
-  }
-
-  return null;
-}
-
+import { Button, Dropdown, Icon, Modal, Nav } from "rsuite";
+import { AppHeader, ResourceListTable } from "@commure/components-core";
+import "./ScheduleService.scss";
+import { FhirDataQuery } from "@commure/components-data";
+import { Bundle, Resource } from "@commure/fhir-types/r4/types";
 function Schedule() {
   const [isOpen, setOpen] = useState(false);
-  const toggleCollapse = () => {
-    setOpen(!isOpen);
+  const [modalVal, setModalVal] = useState(false);
+  const modalClose = () => {
+    setOpen(false);
   };
-  return <><AppHeader showFullUserName={true} logo={<Nav>
-    <img src={logo} alt="Quick Shift Logo" width="40" height="40" />
-    <Nav.Item href='/'>
-      <h6>Quick Shift</h6>
-    </Nav.Item>
-    <Nav.Item href='/home' icon={<Icon icon="home" />}>Home</Nav.Item>
-    <Nav.Item href='/schedule' active={true}>Schedule</Nav.Item>
-    <Nav.Item href='/employee'>Employees</Nav.Item>
-    <Nav.Item href='/skills'>Skills</Nav.Item>
-    <Nav.Item href='/adjustments'>Adjustments</Nav.Item>
-    <Dropdown title="About">
-      <Dropdown.Item href='/company'>Company</Dropdown.Item>
-      <Dropdown.Item href='/team'>Team</Dropdown.Item>
-    </Dropdown>
-  </Nav>} />
-    <ScheduleService />
-  </>;
+  const modalOpen = () => {
+    setOpen(true);
+  }
+  const setModal = (modal: boolean) => {
+    setModalVal(modal);
+  }
+  const onApprove = () => {
+    setModal(true);
+    modalOpen();
+  }
+  const onReject = () => {
+    setModal(false);
+    modalOpen();
+  }
+  
+  return (
+    <>
+      <AppHeader
+        showFullUserName={true}
+        logo={
+          <Nav>
+            <img src={logo} alt="Quick Shift Logo" width="40" height="40" />
+            <Nav.Item href="/">
+              <h6>Quick Shift</h6>
+            </Nav.Item>
+            <Nav.Item href="/home" icon={<Icon icon="home" />}>
+              Home
+            </Nav.Item>
+            <Nav.Item href="/schedule" active={true}>
+              Schedule
+            </Nav.Item>
+            <Nav.Item href="/employee">Employees</Nav.Item>
+            <Nav.Item href="/skills">Skills</Nav.Item>
+            <Nav.Item href="/adjustments">Adjustments</Nav.Item>
+            <Dropdown title="About">
+              <Dropdown.Item href="/company">Company</Dropdown.Item>
+              <Dropdown.Item href="/team">Team</Dropdown.Item>
+            </Dropdown>
+          </Nav>
+        }
+      />
+      <FhirDataQuery queryString="Practitioner?_has:Schedule:actor:active=true&_revinclude=Schedule:actor">
+        {({ data, error, loading }) => {
+          if (loading) {
+            return "Loading...";
+          }
+          if (error) {
+            return "Error loading data!";
+          }
+          let schedules: Resource[];
+          if ((data as Bundle).entry === undefined) schedules = [];
+          else {
+            schedules = (data as Bundle).entry!.map(
+              (value) => value.resource as Resource
+            );
+          }
+
+          return (
+            <ResourceListTable
+              className="full-table"
+              resources={schedules}
+              headerToCellDisplay={{
+                Practitioner: "name",
+                Status: "active",
+                "Time Start": "planningHorizon.start",
+                "Time End": "planningHorizon.end",
+                Action: (_schedule: any) => (
+                  <>
+                    <Button onClick={onApprove} color="green">Approve</Button>
+                    {"     "}
+                    <Button onClick={onReject} color="red">Reject</Button>
+                  </>
+                ),
+              }}
+            />
+          );
+        }}
+      </FhirDataQuery>
+      <Modal show={isOpen} onHide={modalClose}>
+        <Modal.Header>
+          <Modal.Title>Confirm action</Modal.Title>
+        </Modal.Header>
+        {modalVal &&
+        <Modal.Body>Are you sure you want to approve this request?</Modal.Body>}
+        {!modalVal &&
+        <Modal.Body>Are you sure you want to reject this request?</Modal.Body>
+        }
+        <Modal.Footer>
+          <Button onClick={modalClose} appearance="primary">
+            OK
+          </Button>
+          <Button onClick={modalClose} appearance="subtle">
+            Cancel
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
 }
 
 export default Schedule;
--- src/ScheduleService.tsx
+++ src/ScheduleService.tsx
@@ -3,32 +3,11 @@ import { FhirDataQuery, FhirDataQueryConsumer } from "@commure/components-data";
 import { Resource, Bundle } from "@commure/fhir-types/r4/types";
 import React from "react";
 import { useEffect, useState } from "react";
-import "./ScheduleService.scss"
+import { Button } from "rsuite";
+import Tag from "rsuite/lib/Tag";
 
-const ScheduleService: React.FC = () => (
-  <FhirDataQuery queryString="Schedule?practitioner=0458d419-da18-4d86-82de-31a2272f018a_include=Schedule:practitioner">
-  {({ data, error, loading }) => {
-     if (loading) {
-      return "Loading...";
-    }
-    if (error) {
-      return "Error loading data!";
-    }
-    if ((data as Bundle).entry) console.log((data as Bundle).entry![0]);
-    const schedules: Resource[] = (data as Bundle).entry!.map(
-      value => value.resource as Resource
-    );
-    return (
-      <ResourceListTable className="full-table"
-        resources={schedules}
-        headerToCellDisplay={{
-          Practitioner: "name",
-          Status: "active",
-          "Time Start": "planningHorizon.start",
-          "Time End": "planningHorizon.end"
-        }} />
-    );
-  }}
-  </FhirDataQuery>
-);
+
+const ScheduleService = () => {
+  
+}
 export default ScheduleService;
